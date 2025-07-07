9a88b57d307b44fd5b811fc3c7f62f00f0fdd3c9
JohnSinny <tjknu@MSI.lan>
2025-07-06 18:51:32 -0700

    small mistake in the last gravity effect

--- src/main/java/net/mcreator/minepiecegoatededition/potion/GravityMobEffect.java
+++ src/main/java/net/mcreator/minepiecegoatededition/potion/GravityMobEffect.java
@@ -2,13 +2,23 @@ package net.mcreator.minepiecegoatededition.potion;
 
 import net.minecraft.potion.EffectType;
 import net.minecraft.potion.Effect;
+import net.minecraft.item.ItemStack;
 import net.minecraft.entity.LivingEntity;
 
 import net.mcreator.minepiecegoatededition.procedures.TestOnEffectActiveTickProcedure;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class GravityMobEffect extends Effect {
 	public GravityMobEffect() {
 		super(EffectType.NEUTRAL, -1);
+	}
+
+	@Override
+	public List<ItemStack> getCurativeItems() {
+		ArrayList<ItemStack> cures = new ArrayList<ItemStack>();
+		return cures;
 	}
 
 	@Override
